8126975046a9868375f39ed5692eb0fd7a52bc56
ljacks20 <ljacks@college.harvard.edu>
2022-11-10 00:28:25 -0500

    add data to main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-let UsaMap;
+let usaMap;
 
 let promises = [
     //JSON States map projection
     d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json"), //dataArray[0]
-
+    d3.csv('data/merged_data.csv')
 ]
 
 Promise.all(promises)
+<<<<<<< Updated upstream
     .then(function(){})
+=======
+    .then(function(data){
+        console.log(data)
+        initMainPage(data)
+    })
+>>>>>>> Stashed changes
     .catch(function (err){
         console.log(err)
     });
