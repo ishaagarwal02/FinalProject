daee0f44f477275c2bda376399b0987598191f08
ishaagarwal02 <ishaagarwal@college.harvard.edu>
2022-11-10 17:07:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let usaMap;
 let promises = [
     //JSON States map projection
     d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json"), //dataArray[0]
-    d3.csv('data/merged_data.csv')
+    d3.csv('data/merged_data.csv') //dataArray[1]
 ]
 
 Promise.all(promises)
