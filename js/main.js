086204c1d9a8468cb06738b23cbd071d7c23ea39
ishaagarwal02 <ishaagarwal@college.harvard.edu>
2022-11-11 16:07:01 -0500

    adding a selector for the map

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let usaMap;
 let matrixChart;
 
+//uploading the data
 let promises = [
     //JSON States map projection
     d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json"), //dataArray[0]
     d3.csv('data/merged_data.csv')
 ]
 
+//running through the data to then do things with it
 Promise.all(promises)
   
     .then(function(data){
@@ -18,12 +20,19 @@ Promise.all(promises)
         console.log(err)
     });
 
+//initializing the main page of the website
 function initMainPage(allDataArray){
     usaMap = new MapVis('mapDiv', allDataArray[0])
     matrixChart = new MatrixChart('matrixvis', allDataArray[1]);
 
 };
 
+//updating the matrix
 function switchMatrixView() {
     matrixChart.updateChart();
+}
+
+//category change for the map selector
+function categoryChange(){
+
 }
